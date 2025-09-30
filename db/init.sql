de038c4c3dd38fe87cff6b700736af9142b39174
JoaquinTorres <joaqutorres@alumnos.uai.cl>
2025-09-30 13:09:02 -0300

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -33,8 +33,7 @@ CREATE TABLE IF NOT EXISTS stock(
   serial TEXT NULL,
   expiry DATE NULL,
   location TEXT NOT NULL DEFAULT 'MAIN',
-  qty INT NOT NULL,
-  UNIQUE(item_code, COALESCE(lot,''), COALESCE(serial,''), location)
+  qty INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS moves(
